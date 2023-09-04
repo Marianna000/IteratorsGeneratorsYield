9b91d07326bada98f8e6512d0928a34f7513b515
Your Name <marinabananas@gmail.com>
2023-09-05 00:58:55 +0300

    second

--- generator.py
+++ generator.py
@@ -1,11 +1,14 @@
+import types
+
+
 def flat_generator(list_of_lists):
-    new_list = []
+    #new_list = []
     for val in list_of_lists:
         for item in val:
-            new_list.append(item)
-    yield new_list
+            #new_list.append(item)
+            yield item
 
-    print(new_list)
+
 
 
 
@@ -16,6 +19,8 @@ def test_2():
         ['d', 'e', 'f', 'h', False],
         [1, 2, None]
     ]
+
+#    print(list(flat_generator(list_of_lists_1)))
 
     for flat_iterator_item, check_item in zip(
             flat_generator(list_of_lists_1),
--- iterator.py
+++ iterator.py
@@ -2,18 +2,25 @@ class FlatIterator:
 
     def __init__(self, list_of_list):
         self.list = list_of_list
+        self.len = len(list_of_list) #3
 
 
     def __iter__(self):
         print('start of the cycle')
+        self.counter = -1
         self.values = []
         return self
 
     def __next__(self):
+        self.counter += 1
 
-        for val in self.list:
-            for item in val:
-                self.values.append(item)
+        for val in self.list[self.counter]:
+            self.values.append(val)
+
+        if self.counter <= self.len:
+            raise StopIteration
+
+
         return self.values
 
 def test_1():
@@ -23,6 +30,8 @@ def test_1():
         ['d', 'e', 'f', 'h', False],
         [1, 2, None]
     ]
+
+    print(list(FlatIterator(list_of_lists_1)))
 
     for flat_iterator_item, check_item in zip(
             FlatIterator(list_of_lists_1),
