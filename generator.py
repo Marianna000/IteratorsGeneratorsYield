eb4f8f65eab96b9c1f450196ca827b3f9687a467
Your Name <marinabananas@gmail.com>
2023-09-05 00:58:55 +0300

    third

--- generator.py
+++ generator.py
@@ -1,11 +1,13 @@
+import types
+
+
 def flat_generator(list_of_lists):
-    new_list = []
+
     for val in list_of_lists:
         for item in val:
-            new_list.append(item)
-    yield new_list
+            yield item
 
-    print(new_list)
+
 
 
 
@@ -16,6 +18,7 @@ def test_2():
         ['d', 'e', 'f', 'h', False],
         [1, 2, None]
     ]
+
 
     for flat_iterator_item, check_item in zip(
             flat_generator(list_of_lists_1),
--- iterator.py
+++ iterator.py
@@ -1,20 +1,42 @@
+from itertools import chain
+
+
 class FlatIterator:
 
     def __init__(self, list_of_list):
         self.list = list_of_list
+        self.counter = 0
+        self.storage = []
 
 
     def __iter__(self):
         print('start of the cycle')
-        self.values = []
         return self
+
 
     def __next__(self):
 
-        for val in self.list:
-            for item in val:
-                self.values.append(item)
-        return self.values
+        if self.counter >= len(self.list):
+            if self.storage:
+                # Дошли до конца вложенного списка,
+                # выходим из него во внешний, "вспоминаем" состояние
+                self.list, self.counter = self.storage.pop()
+                return next(self)
+            else:
+                raise StopIteration
+        item = self.list[self.counter]
+        self.counter += 1
+
+        if type(item) is not list:
+            return item
+        else:
+            # Запоминаем текущее состояние
+            self.storage.append((self.list, self.counter))
+            # Входим во вложенный список, он становится текущим списком
+            self.list = item
+            self.counter = 0
+            return next(self)
+
 
 def test_1():
 
@@ -23,6 +45,8 @@ def test_1():
         ['d', 'e', 'f', 'h', False],
         [1, 2, None]
     ]
+
+    print(list(FlatIterator(list_of_lists_1)))
 
     for flat_iterator_item, check_item in zip(
             FlatIterator(list_of_lists_1),
